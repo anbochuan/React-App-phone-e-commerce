3ee2562f6ec0a6fe1e194e4454476d5455b32bda
anbochuan <anbochuan@gmail.com>
2019-07-05 10:38:57 -0400

    integrated with PayPal sandbox

--- my-react-app/src/components/Cart/Cart.js
+++ my-react-app/src/components/Cart/Cart.js
@@ -20,7 +20,7 @@ export default class Cart extends Component {
                                     <Title name="your" title="cart"></Title>
                                     <CartColumns></CartColumns>
                                     <CartList value={value}></CartList>
-                                    <CartTotals value={value}></CartTotals>
+                                    <CartTotals value={value} history={this.props.history}></CartTotals>
                                 </React.Fragment> 
                             );
                         } else {
